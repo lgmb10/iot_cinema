f593e75663d86b130503582ae8445f8ea6586648
Mathis Kazek <>
2023-06-22 22:52:41 +0200

    Add activate clim or deshumid or sound check

--- back/utils/mqttHelper.js
+++ back/utils/mqttHelper.js
@@ -1,38 +1,28 @@
 const Sensor = require("../models/sensor")
+const sensorRange = require("../models/sensorRange")
 
 exports.initMqtt = (client) => {
     console.log('Connecting to MQTT broker...')
 
     client.on('connect', function () {
-        client.subscribe('room/1/#', function (err) {
+        client.subscribe('room/#', function (err) {
             if (!err) {
-                console.log('Subscribed to "room 1" channel successfully!')
+                console.log('Subscribed to "room" channel successfully!')
             } else {
-                console.error('Error subscribing to "room 1" channel:', err)
-            }
-        })
-
-        client.subscribe('room/2/#', function (err) {
-            if (!err) {
-                console.log('Subscribed to "room 2" channel successfully!')
-            } else {
-                console.error('Error subscribing to "room 1" channel:', err)
-            }
-        })
-
-        client.subscribe('room/3/#', function (err) {
-            if (!err) {
-                console.log('Subscribed to "room 3" channel successfully!')
-            } else {
-                console.error('Error subscribing to "room 1" channel:', err)
+                console.error('Error subscribing to "room" channel:', err)
             }
         })
     })
 
-    client.on('message', function (topic, message) {
+    client.on('message', async function (topic, message) {
         console.log('Received message on topic "' + topic + '": ' + message.toString())
-        console.log({ room: topic.split('/')[1], sensorType: topic.split('/')[2], sensorValue: message.toString() })
-        //Sensor.create({room: topic.split('/')[1], sensorType: topic.split('/')[2], sensorValue: message.toString()})
-
+        const range = await sensorRange.findOne({ room: parseInt(topic.split('/')[1]), sensorType: topic.split('/')[2] })
+        if (parseInt(message.toString()) < range.sensorValueMin) {
+            client.publish(`range/${topic.split('/')[1]}/${topic.split('/')[2]}/min`, `${range.sensorValueMin}/${range.sensorValueMax}`)
+        } else if (parseInt(message.toString()) > range.sensorValueMax) {
+            client.publish(`range/${topic.split('/')[1]}/${topic.split('/')[2]}/max`, `${range.sensorValueMin}/${range.sensorValueMax}`)
+        } else {
+            Sensor.create({room: topic.split('/')[1], sensorType: topic.split('/')[2], sensorValue: message.toString()})
+        }
     })
 }
--- sensors/server.js
+++ sensors/server.js
@@ -13,29 +13,28 @@ let client = mqtt.connect(options)
 
 client.on('connect', function () {
     console.log('Connected')
-    client.subscribe('room/1/#', function (err) {
+    client.subscribe('range/#', function (err) {
         if (!err) {
-            console.log('Subscribed to "room 1" channel successfully!')
+            console.log('Subscribed to "range" channel successfully!')
         } else {
-            console.error('Error subscribing to "room 1" channel:', err)
+            console.error('Error subscribing to "range" channel:', err)
         }
     })
+})
 
-    client.subscribe('room/2/#', function (err) {
-        if (!err) {
-            console.log('Subscribed to "room 2" channel successfully!')
-        } else {
-            console.error('Error subscribing to "room 1" channel:', err)
-        }
-    })
-
-    client.subscribe('room/3/#', function (err) {
-        if (!err) {
-            console.log('Subscribed to "room 3" channel successfully!')
-        } else {
-            console.error('Error subscribing to "room 1" channel:', err)
-        }
-    })
+client.on('message', async function (topic, message) {
+    console.log('Received message on topic "' + topic + '": ' + message.toString())
+    const isMin = topic.split('/')[3] === 'min'
+    const selectedSensor = topic.split('/')[2]
+    if (isMin) {
+        if (selectedSensor === 'temperature') {client.publish(`room/${topic.split('/')[1]}/${topic.split('/')[2]}`, `${temperatureValue + 0.5}`); temperatureValue += 0.5}
+        if (selectedSensor === 'sound') {client.publish(`room/${topic.split('/')[1]}/${topic.split('/')[2]}`, `${soundValue + 1}`); soundValue += 1}
+        if (selectedSensor === 'humidity') {client.publish(`room/${topic.split('/')[1]}/${topic.split('/')[2]}`, `${humidityValue + 10}`); humidityValue += 10}
+    } else {
+        if (selectedSensor === 'temperature') {client.publish(`room/${topic.split('/')[1]}/${topic.split('/')[2]}`, `${temperatureValue - 0.5}`); temperatureValue -= 0.5}
+        if (selectedSensor === 'sound') {client.publish(`room/${topic.split('/')[1]}/${topic.split('/')[2]}`, `${soundValue - 10}`); soundValue -= 10}
+        if (selectedSensor === 'humidity') {client.publish(`room/${topic.split('/')[1]}/${topic.split('/')[2]}`, `${humidityValue - 1}`); humidityValue -= 1}
+    }
 })
 
 let temperatureValue = 22;
@@ -61,17 +60,17 @@ function simulateValue(min, max, variation, currentValue) {
 
 setInterval(() => {
     // Temperature
-    client.publish('room/1/temperature', simulateValue(20, 30, 0.5, temperatureValue).toFixed(1))
-    client.publish('room/2/temperature', simulateValue(20, 30, 0.5, temperatureValue).toFixed(1))
-    client.publish('room/3/temperature', simulateValue(20, 30, 0.5, temperatureValue).toFixed(1))
+    client.publish('room/1/temperature', simulateValue(15, 35, 0.5, temperatureValue).toFixed(1))
+    client.publish('room/2/temperature', simulateValue(15, 35, 0.5, temperatureValue).toFixed(1))
+    client.publish('room/3/temperature', simulateValue(15, 35, 0.5, temperatureValue).toFixed(1))
 
     // Humidity
-    client.publish('room/1/humidity', simulateValue(45, 65, 0.5, humidityValue).toFixed(1))
-    client.publish('room/2/humidity', simulateValue(45, 65, 0.5, humidityValue).toFixed(1))
-    client.publish('room/3/humidity', simulateValue(45, 65, 0.5, humidityValue).toFixed(1))
+    client.publish('room/1/humidity', simulateValue(35, 75, 1, humidityValue).toFixed(1))
+    client.publish('room/2/humidity', simulateValue(35, 75, 1, humidityValue).toFixed(1))
+    client.publish('room/3/humidity', simulateValue(35, 75, 1, humidityValue).toFixed(1))
 
     // Sound
-    client.publish('room/1/sound', simulateValue(65, 117, 25, soundValue).toFixed(1))
-    client.publish('room/2/sound', simulateValue(65, 117, 0.5, soundValue).toFixed(1))
-    client.publish('room/3/sound', simulateValue(65, 117, 0.5, soundValue).toFixed(1))
+    client.publish('room/1/sound', simulateValue(60, 125, 25, soundValue).toFixed(1))
+    client.publish('room/2/sound', simulateValue(60, 125, 25, soundValue).toFixed(1))
+    client.publish('room/3/sound', simulateValue(60, 125, 25, soundValue).toFixed(1))
 }, 5 * 1000);  // Exécute simulateValue chaque 2 minutes
